dbfc879d9a39a3a9a85f9e1e525259129b8d88f7
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-05-26 19:26:22 +0530

    Create SubArray.java

---
+++ 26May/SubArray.java
@@ -0,0 +1,36 @@
+public class Main { 
+    // function to find total sub-array 
+    // which start and end with same element 
+    public static void cntArray(int A[], int N) 
+    { 
+        // initialize result with 0 
+        int result = 0; 
+  
+        for (int i = 0; i < N; i++) { 
+  
+            // all size 1 sub-array 
+            // is part of our result 
+            result++; 
+  
+            // element at current index 
+            int current_value = A[i]; 
+  
+            for (int j = i + 1; j < N; j++) { 
+  
+                // Check if A[j] = A[i] 
+                // increase result by 1 
+                if (A[j] == current_value) { 
+                    result++; 
+                } 
+            } 
+        } 
+        System.out.println(result); 
+    } 
+    public static void main(String[] args) 
+    { 
+        int[] A = { 1, 5, 6, 1, 9, 
+                    5, 8, 10, 8, 9 }; 
+        int N = A.length; 
+        cntArray(A, N); 
+    } 
+} 
