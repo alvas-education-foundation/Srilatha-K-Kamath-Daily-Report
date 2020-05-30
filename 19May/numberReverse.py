598e85047ba4d2a534d241a84d007551bbba6e13
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-05-30 08:47:45 +0530

    Delete numberReverse.py

--- 19May/numberReverse.py
+++
@@ -1,8 +0,0 @@
-# Python Program to Reverse a Number
-number = int(input("Please Enter any Number: "))
-Reverse = 0
-while(number > 0):
-    Reminder = number %10
-    Reverse = (Reverse *10) + Reminder
-    number = number //10
-print("\nReverse of entered number is = %d" %Reverse)
