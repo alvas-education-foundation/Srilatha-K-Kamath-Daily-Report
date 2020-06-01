0d59766b24a45449e9723a8fc58b5bca770f5434
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-06-01 20:08:01 +0530

    Update duplicate.py

--- June01/duplicate.py
+++ June01/duplicate.py
@@ -1,7 +1,4 @@
 #Python Program to remove duplicate elements from a list
- Description:
-input[1,2,3,3,4,5,5,6]
-Output[1,2,3,4,5,6]
 a=[]
 n= int(input("Enter the number of elements in list:"))
 for x in range(0,n):
