d0353b1d5bd626a366c6bfb3d0bed31da90e1247
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-06-08 19:11:25 +0530

    Update PalindromeCheck.py

--- June08/PalindromeCheck.py
+++ June08/PalindromeCheck.py
@@ -1,8 +1,13 @@
-def palindromecheck(string):
-    if(string == string[:: - 1]):
-        print("True")
+def palindromechk(s):
+    if len(s) < 1:
+        return True
     else:
-        print("False")
-#main
-string = input("Enter the String : ")
-palindromecheck(string)
+        if s[0] == s[-1]:
+            return palindromechk(s[1:-1])
+        else:
+            return False
+string=input("Enter string:")
+if(palindromechk(string)==True):
+    print("String is a palindrome!")
+else:
+    print("String isn't a palindrome!")
