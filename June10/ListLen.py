803deebf5d5bb6a5db8f425ce21c2f70fa64a9e3
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-06-10 19:25:02 +0530

    Update ListLen.py

--- June10/ListLen.py
+++ June10/ListLen.py
@@ -3,5 +3,5 @@ def length(lst):
         return 0
     return 1 + length(lst[1::2]) + length(lst[2::2])
 a=[1,2,3,4,5,6,7,8,9,10]
-print("Length of the string is: ",)
+print("Length of the list is: ",)
 print(length(a))
