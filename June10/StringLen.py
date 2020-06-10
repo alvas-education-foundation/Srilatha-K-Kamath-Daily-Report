75bc825391b66ddb5e5cc364cc0e354a7cdadba6
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-06-10 18:27:49 +0530

    Update and rename StringLen.py to ListLen.py

--- June10/StringLen.py
+++
@@ -1,8 +0,0 @@
-def string_length(string) :
-    if string == '': 
-        return 0
-    else : 
-        return 1 + string_length(string[1:])  
-#main 
-string=input('Enter string:')
-print (string_length(string)) 
