d8d835e339cb1c114355dccb2002806157267cfe
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-06-11 17:55:46 +0530

    Create LowerToUpper.py

---
+++ June11/LowerToUpper.py
@@ -0,0 +1,10 @@
+def to_uppercase(str1):
+    num_upper = 0
+    for letter in str1[:4]: 
+        if letter.upper() == letter:
+            num_upper += 1
+    if num_upper >= 2:
+        return str1.upper()
+    return str1
+string=input('Enter string:')
+print(to_uppercase(string))
