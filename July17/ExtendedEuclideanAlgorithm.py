093d8d7269dea926824fdf3007897fe86af2be1c
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-07-17 18:01:38 +0530

    Create ExtendedEuclideanAlgorithm.py

---
+++ July17/ExtendedEuclideanAlgorithm.py
@@ -0,0 +1,11 @@
+def gcdExtended(a, b):
+    if a == 0 :
+        return b,0,1
+    gcd,x1,y1 = gcdExtended(b%a, a)
+    x = y1 - (b//a) * x1
+    y = x1
+    return gcd,x,y
+a=int(input('Enter a: '))
+b=int(input('Enter b: '))
+g, x, y = gcdExtended(a, b)
+print("gcd(", a , "," , b, ") = ", g)
