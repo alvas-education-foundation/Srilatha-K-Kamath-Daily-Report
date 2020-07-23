67ac6a8ea444e0e4464cf09f71f1b24204e6cde9
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-07-23 17:44:03 +0530

    Create SumOfFactors.py

---
+++ July23/SumOfFactors.py
@@ -0,0 +1,12 @@
+def findMinSum(num):
+    sum = 0
+    i = 2
+    while(i * i <= num):
+        while(num % i == 0):
+            sum += i
+            num /= i
+        i += 1
+    sum += num
+    return sum
+num = int(input('Enter number:'))
+print(findMinSum(num))
