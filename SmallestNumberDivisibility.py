bf6545451e4f856382738e7395715393be2c11d6
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-07-27 18:38:56 +0530

    Delete SmallestNumberDivisibility.py

--- SmallestNumberDivisibility.py
+++
@@ -1,9 +0,0 @@
-def answer(X, K):
-    MIN = pow(10, K-1)
-    if(MIN%X == 0):
-        return (MIN)
-    else:
-        return ((MIN + X) - ((MIN + X) % X))
-X = 83;  
-K = 5;  
-print(answer(X, K));
