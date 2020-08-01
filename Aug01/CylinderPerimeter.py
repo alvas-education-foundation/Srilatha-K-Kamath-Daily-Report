33358553eb8d863628cac4dc79d6eeedcd2baa8b
SriLathaK850 <53564473+SriLathaK850@users.noreply.github.com>
2020-08-01 19:46:55 +0530

    Create CylinderPerimeter.py

---
+++ Aug01/CylinderPerimeter.py
@@ -0,0 +1,6 @@
+def perimeter(diameter,height ) : 
+    return 2*(diameter+height )  
+# main 
+diameter = int(input('Enter diameter:'))
+height = int(input('Enter height:'))
+print ("Perimeter = ",perimeter(diameter, height),'units') 
